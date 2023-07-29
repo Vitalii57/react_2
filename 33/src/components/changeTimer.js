720f8d126499a0d9cf34833184c336ec7017cbc8
Vitalii57 <zubkov127@gmail.com>
2023-07-29 14:38:31 +0300

    task

---
+++ 33/src/components/changeTimer.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+import React, { Component } from 'react'
+
+export default class changeTimer extends Component {
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+        hour: timeHour.textContent,
+        minute: timeMinute.textContent,
+        second: timeSecond.textContent,
+      }
+    }
+    
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
